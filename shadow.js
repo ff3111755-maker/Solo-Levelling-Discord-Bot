8ec16cca7229ee5d9f7f2d5003914cd0e76e8507
ff3111755-maker <ff3111755@gmail.com>
2026-02-15 16:03:31 +0530

    Add shadow.js with character data

---
+++ shadow.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { id: "igris", name: "Igris", rarity: "Legendary", image: "https://link-to-igris.png", maxPerUser: 3 },
+  { id: "iron", name: "Iron", rarity: "Rare", image: "https://link-to-iron.png", maxPerUser: 3 },
+  { id: "garm", name: "Garm", rarity: "Epic", image: "https://link-to-garm.png", maxPerUser: 3 }
+];
